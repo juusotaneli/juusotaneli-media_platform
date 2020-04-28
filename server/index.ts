0ee2592d49df75e4485243575f916c6e66f3ce5d
juusotaneli <juuso.karvonen@outlook.com>
2020-04-28 21:55:56 +0300

    heroku fix

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-
 const app = express();
+app.use(express.static('ui/build'));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'));
 
 
 app.get('/ping', (_req, res) => { 
