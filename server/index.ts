a9e5fe98005c805c8194797c2025290a5b2215a2
juusotaneli <juuso.karvonen@outlook.com>
2020-04-28 12:49:29 +0300

    heroku fixes

--- server/index.ts
+++ server/index.ts
@@ -5,13 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3001;
 
 app.get('/ping', (_req, res) => { 
   console.log('someone pinged here');
   res.send('YO MAN');
 });
   
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
