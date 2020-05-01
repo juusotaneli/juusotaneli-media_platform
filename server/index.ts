28d46fd95e77b69a0f7f30be8af522cdb093cf4f
juusotaneli <juuso.karvonen@outlook.com>
2020-05-01 13:17:54 +0300

    previous commits didnt include the changes in the backend

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import express from 'express';
 import cors from 'cors';
 const app = express();
 app.use(express.static('ui/build'));
 app.use(express.json());
 app.use(cors());
-
+// eslint-disable-next-line no-undef
+require('dotenv').config();
+// eslint-disable-next-line no-undef
+const CurrentsAPI = require('currentsapi');
+// eslint-disable-next-line no-undef
+const currentsapi = new CurrentsAPI(`${process.env.CURRENTS_API_KEY}`);
 
 app.get('/ping', (_req, res) => { 
-  console.log('someone pinged here');
-  res.send('YO MAN');
+  currentsapi.search({
+    keywords: 'health',
+  }).then((response: unknown) => {
+      console.log(response);
+      res.send(response);
+  });
 });
-  
+
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
