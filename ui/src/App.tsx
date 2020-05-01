40b3c25a3805ba5a3b9e1d2261198ae3fa31d754
juusotaneli <juuso.karvonen@outlook.com>
2020-05-01 13:13:53 +0300

    heroku fix

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
       setS(promise.data.news);
     };
     fetchData();
-  }, [s]);
+  }, [setS]);
 
   console.log(s);
   if (s) {
@@ -27,7 +27,7 @@ const App: React.FC = () => {
     );
   } return (
     <>
-    {console.log(s)}
+      {console.log(s)}
       loading...
     </>
   );
